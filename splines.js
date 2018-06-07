f649dc3db46003c18f455560e5767d5dab141c4a
LucasCCabral <lcc4@cin.ufpe.br>
2018-06-07 11:19:34 -0300

    Implementação de curvas de bezier com entrada do usuário.

--- splines.js
+++ splines.js
@@ -38,9 +38,9 @@ function drawLine(){
 //descobrir como enviar um objeto para função, sem sser global
 function drawLine1(){
 	ctx.beginPath();
-	ctx.strokeStyle = 'black';
+	ctx.strokeStyle = 'red';
 	ctx.lineWidth = '5';
-	ctx.fillStyle = 'black';
+	ctx.fillStyle = 'red';
 	ctx.moveTo(aux1.x,aux1.y);
 	ctx.lineTo(aux2.x,aux2.y);
 	ctx.stroke();
@@ -60,14 +60,20 @@ function bezierCurve(){
 		c = Math.pow(t,2);
 		d = Math.pow(t,3);
 
-		x = a*p1.x + 3*b*t*p2.x+ 3*c*(1-t)*p3.x + d*p4.x;
-    	y = a*p1.y + 3*b*t*p2.y+ 3*c*(1-t)*p3.y + d*p4.y;
+		x = a*controlPointx[i-3] + 3*b*t*controlPointx[i-2]+ 3*c*(1-t)*controlPointx[i-1] + d*controlPointx[i];
+		y = a*controlPointy[i-3] + 3*b*t*controlPointy[i-2]+ 3*c*(1-t)*controlPointy[i-1] + d*controlPointy[i];
     	//-----------------------------------------------------------------
 
     	aux2.x = aux1.x;
     	aux2.y = aux1.y;
     	aux1.x = x;
     	aux1.y = y;
+    	
+    	if(t === 0 ){
+    		aux2.x = aux1.x;
+    		aux2.y = aux1.y;
+    	}
+    	
     	drawLine1();
 	}
 }
@@ -76,30 +82,36 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 resizeToFit();
 //colocar como variável local em bezeierCurve, assim que descobrir como enviar variáveis locais
-var aux1 = new Dot(50,50);
+var aux1 = new Dot(0,0);
 var aux2 = new Dot(0,0);
 //playing with arrays
 var p1 = new Dot(0,0);
 var p2 = new Dot(0,0);
-var controlPoints = new Array;
+var controlPointx = new Array;
+var controlPointy = new Array;
 var i = 0;
 var j = 0;
 
-//estou salvando um ponteiro para p1 e não os seus valores em si.
 canvas.addEventListener('mousedown', function(e) {
 	p1.x=e.offsetX;
     p1.y=e.offsetY;
-    controlPoints.push(p1); //quero passar os valores apenas
-
+    controlPointx.push(p1.x);
+    controlPointy.push(p1.y);
     if(i>0){
-    	p2 = controlPoints[j];
+    	p2.x = controlPointx[j];
+    	p2.y = controlPointy[j];
     	drawLine();
     }
-	
-	console.log("p1.x= " + controlPoints[i].x + "p1.y= " + controlPoints[i].y);
-	console.log("p2.x= " + controlPoints[j].x + "p2.y= " + controlPoints[j].y);    
-
-    drawDot();
+//console.log("p1.x= " + p1.x + " " + "p1.y= " + p1.y);
+//console.log("p2.x= " + " " + p2.x + "p2.y= "+ p2.y);    
+    drawDot();    
+    
+    if(i === 3){
+    	for(var k = 0; k < controlPointx.length;k++){
+    		console.log("p%d.x=" + controlPointx[k] + "p%d.y"+controlPointy[k] ,k,k);
+    	}
+    	bezierCurve();
+    }
     
     j=i;
     i++;
